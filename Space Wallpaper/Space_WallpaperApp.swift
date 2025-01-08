ed46ab6e5f49a23aa8f98acfabfce0fa3d3be74e
dhruba <adhroba@gmail.com>
2025-01-09 01:26:47 +0600

    .gitignore file added

---
+++ Space Wallpaper/Space_WallpaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Space_WallpaperApp.swift
+//  Space Wallpaper
+//
+//  Created by Abu Loman Hossain Shuvo on 1/9/25.
+//
+
+import SwiftUI
+
+@main
+struct Space_WallpaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
